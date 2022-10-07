c58ba138cd1e7de33b718a26571dc87d88626734
feluelle <felix.uellendall@astronomer.io>
2022-10-07 10:22:04 +0200

    Render sql code with parameters in BaseSQLDecoratedOperator
    - add sql and parameters to BaseSQLDecoratedOperator template_fields
    - add .sql to BaseSQLDecoratedOperator template_ext
    - add check for template_fields and template_ext

--- python-sdk/tests/sql/operators/test_base_decorator.py
+++ python-sdk/tests/sql/operators/test_base_decorator.py
@@ -7,3 +7,12 @@ def test_base_sql_decorated_operator_template_fields_with_parameters():
      as this required for taskflow to work if XCom args are being passed via parameters.
     """
     assert "parameters" in BaseSQLDecoratedOperator.template_fields
+
+
+def test_base_sql_decorated_operator_template_fields_and_template_ext_with_sql():
+    """
+    Test that sql is in BaseSQLDecoratedOperator template_fields and template_ext
+     as this required for rending the sql in the task instance rendered section.
+    """
+    assert "sql" in BaseSQLDecoratedOperator.template_fields
+    assert ".sql" in BaseSQLDecoratedOperator.template_ext
